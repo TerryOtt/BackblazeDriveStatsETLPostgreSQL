4f81fec184f936006083a5dd47bc2d0ce49bb540
Terry Ott <Terry.Ott@gmail.com>
2025-07-14 07:05:46 -0400

    actually reading out of csv channel and doing the right thing

--- main.go
+++ main.go
@@ -2,13 +2,55 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
-func main() {
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
+func readCsvFiles(csvChannel chan string,
+	datapointsChannel chan string,
+	wg *sync.WaitGroup) {
 
-	for i := 1; i <= 5; i++ {
-		fmt.Println("i =", 100/i)
+	fmt.Println("Waiting for CSV filenames over channel")
+
+	// Read from channel until it is closed
+	for csvFile := range csvChannel {
+		fmt.Printf("Reading CSV file %s\n", csvFile)
 	}
+
+	// Have to mark all our work is done before we bail
+	wg.Done()
+}
+
+func main() {
+	fmt.Println("Parsing cmdline args")
+	// Create channel to send CSV to readers
+	var csvWg sync.WaitGroup
+	csvFilenamesChannel := make(chan string)
+
+	// Create channel to send datapoints to writers
+	const (
+		// CHANNEL_SIZE 512 MB of datapoints at 400 bytes/datapoint (roughly)
+		ChannelSize int = (512 * 1024 * 1024) / 400
+	)
+	datapointsChannel := make(chan string, ChannelSize)
+
+	fmt.Println("Starting reader workers")
+	const NumReaders int = 1
+	for i := 0; i < NumReaders; i++ {
+		csvWg.Add(1)
+		go readCsvFiles(csvFilenamesChannel, datapointsChannel, &csvWg)
+	}
+	fmt.Println("Starting writer workers")
+	fmt.Println("Finding CSV files")
+	// Send all CSV files over the channel to the readers
+	csvFilenamesChannel <- "somefile.csv"
+
+	// Close the channel to bring readers home
+	close(csvFilenamesChannel)
+	fmt.Println("CSV reads have kicked off")
+	// Wait on reader waitgroup
+	csvWg.Wait()
+	fmt.Println("All datapoints have been read, waiting for writes to complete")
+	// Poison pill the writers
+	// Wait for writers waitgroup to finish
+	fmt.Println("All datapoints have been successfully written")
 }
